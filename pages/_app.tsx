94d7724a2a64a09694674c7712a3699841e5e4c0
Iván Müller <ivanmuller2.0@gmail.com>
2022-05-27 19:40:04 -0300

    disable right click

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Footer from 'components/Footer'
 
 function MyApp ({ Component, pageProps }: AppProps) : JSX.Element {
+  useEffect(() => {
+    window.addEventListener('contextmenu', (e) => { e.preventDefault(); alert('Sorry, you\'re not allowed to cheat 🤣') })
+  }, [])
   return (
     <ChakraProvider>
       <SWRConfig value={{
