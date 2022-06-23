1b821c9e1e729cbab5a95259436ef8be95b33a94
Iván Müller <ivanmuller2.0@gmail.com>
2022-06-23 02:17:46 -0300

    ts fix | date update

--- settings.ts
+++ settings.ts
@@ -5,7 +5,7 @@ const settings = {
   urls: {
     getMovie: (movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=${settings.movieDbApiKey}`,
     getImages: (movieId) => `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${settings.movieDbApiKey}&include_image_language=null`,
-    getMovieset1: (pageNumber) => `https://api.themoviedb.org/3/discover/movie?api_key=${settings.movieDbApiKey}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${pageNumber}&primary_release_date.gte=1980-01-01&primary_release_date.lte=2022-04-30&with_release_type=1&vote_count.gte=1000&vote_average.gte=7&with_original_language=en`,
+    getMovieset1: (pageNumber) => `https://api.themoviedb.org/3/discover/movie?api_key=${settings.movieDbApiKey}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${pageNumber}&primary_release_date.gte=1980-01-01&primary_release_date.lte=2022-05-30&with_release_type=1&vote_count.gte=1000&vote_average.gte=7&with_original_language=en`,
     searchMovies: (inputValue) => `https://api.themoviedb.org/3/search/movie?api_key=${settings.movieDbApiKey}&language=en-US&query=${inputValue}&page=1&include_adult=false`,
     imageBaseBackdrop: (imgFile, width = 1280) => `https://image.tmdb.org/t/p/w${width}${imgFile}`
   },
